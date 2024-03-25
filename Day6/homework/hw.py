07a836828b6208dacdfd80656b0472890627d6f1
iraklidudashvili <rdudashvl@gmail.com>
2024-03-25 18:46:43 +0400

    secend commit

---
+++ Day6/homework/hw.py
@@ -0,0 +1,57 @@
+#
+
+print(5>6 or 7>11)
+
+print(3>6 or 9>5)
+
+print(1<10 or 7>5)
+
+print(9>13 or 5<6)
+
+print(3>1 or 7<3)
+
+print(5==5 and 9>3)
+
+print(3<1 and 10<5)
+
+print(4<1 and 9<6)
+
+print(8<1 and 6<5)
+
+print(9<2 and 12<9)
+
+num1 =True or False #False
+
+num3 =True and True #True
+
+num4 =input("enter a num")
+
+num5 =input("enter a num")
+
+print(num4>num5 or num5>num4)
+
+print(num4>num5 and num5>num4)
+
+num1 =10>19
+
+num3 =11>10
+
+print(num1 or num3)
+
+print(num1 and num3)
+
+name ="nika"
+
+age =13
+
+month =13.3
+
+breath =True
+
+print(type(name))
+
+print(type(age))
+
+print(type(month))
+
+print(type(breath))
---
+++ Day7/homework/hw.py
@@ -0,0 +1,37 @@
+name ="nika"
+
+age =13
+
+month =13.3
+
+breath =True
+
+print(type(name))
+
+print(type(age))
+
+print(type(month))
+
+print(type(breath))
+
+num1 = 5<11
+
+num2 = 5<12
+
+num3 = 5<17
+
+num4 = 7<9
+
+num5 = 9<11
+
+name =True and False
+
+name2 =True or False
+
+name3 =True and True
+
+name4 =True or True
+
+name5 =False and False
+
+name6 =False or False
