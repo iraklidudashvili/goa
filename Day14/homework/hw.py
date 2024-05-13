115c153ecff22aa0fe99af35a79cc2ebefaa4842
iraklidudashvili <rdudashvl@gmail.com>
2024-05-13 18:13:51 +0400

    irakli commit

---
+++ Day14/homework/hw.py
@@ -0,0 +1,9 @@
+num = int(input("enter a num:"))
+
+if num % 2 == 0:
+    print("number is even")
+
+else:
+    print("number is odd")
+
+#solo learn გვქონდა
---
+++ Day15/classwork/cw.py
@@ -0,0 +1,1 @@
+#solo learn და kahoot გვქონდა
