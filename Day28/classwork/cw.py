fa8741cf133d7e1de1cec07b151570056e609637
iraklidudashvili <rdudashvl@gmail.com>
2024-08-11 18:30:26 +0400

    goa

---
+++ Day28/classwork/cw.py
@@ -0,0 +1,86 @@
+#დავალება 1)  0-იდან 20-ის ჩათვლით გამოიტანეთ რიცხვები
+# for i in range (0,21):
+#     print(i)
+
+
+
+
+#დავალება 2) მომხმარებელს შემოატანინეთ ორი რიცხვი და მათ შორის არსებულები გამოიტანეთ
+# num1=int(input("enter first number:"))
+# num2=int(input("enter second number:"))
+# for i in range(num1,num2):
+#     print(i)
+
+
+
+
+#დავალება 3) 50-იდან 100-ის ჩათვლით გამოიტანეთ რიცხვები
+# for i in range(50,101):
+#     print(i)
+
+
+
+
+#დავალება 4) დაიწყეთ 100-დან და 50-ის ჩათვლით გამოიტანეთ რიცხვები
+# for i in range(100,49,-1):
+#     print(i)
+
+
+
+
+#დავალება 5) 0-იდან 50-ის ჩათვლით გამოიტანეთ რიცხვები და ბოლოს მათი ჯამი
+# num=0
+# for i in range(0,51):
+#     num += i
+# print()
+
+
+
+
+#დავალება 6) მომხმარებელს შეეკითხეთ რომ შემოიტანოს მთელი რიცხვი. შემდეგ 0-დან ამ რიცხვამდე გამოიტანეთ ყველა მთელი რიცხვი
+
+
+
+
+
+
+
+
+
+#7
+# num1=int(input("enter first number:"))
+# num2=int(input("enter second number:"))
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
+# print(num1%num2)
+# print(num1//num2)
+
+
+
+
+#8
+# num1=int(input("enter your age:"))
+# if num1 > 18:
+#     print("you are not 18 years old")
+# else:
+#     print("you are more than 18 years old")
+
+
+
+#9
+# print(5>3)
+# print(5<3)
+# print(5>=5)
+# print(5<=3)
+# print(5!=3)
+# print(5==5)
+
+
+
+
+#10
+# budget = int(input("enter your budget:"))
+# price = int(input("enter item price:"))
+# print(budget>=price)
